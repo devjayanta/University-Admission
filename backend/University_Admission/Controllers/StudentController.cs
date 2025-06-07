72454120e1c6f41b3fb58c76c1119fb641857cfb
uzumakiNabin <nabin.kaucha@gmail.com>
2025-06-07 15:58:46 +0545

    feature(student): added student apis

---
+++ backend/University_Admission/Controllers/StudentController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using University_Admission.Data;
+using University_Admission.Domain.Enum;
+using University_Admission.ViewModel;
+
+namespace University_Admission.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class StudentController
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public StudentController(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        public async Task<ActionResult<Response<List<StudentViewModel>>>> GetAllStudents()
+        {
+            try
+            {
+                var students = await _db
+                    .Users.Where(u => u.Role == Role.Student && u.DeletedAt == null)
+                    .ToListAsync();
+                if (students == null)
+                {
+                    return Response<List<StudentViewModel>>.FailureResponse("No students exist");
+                }
+                return Response<List<StudentViewModel>>.SuccessResponse(
+                    _mapper.Map<List<StudentViewModel>>(students)
+                );
+            }
+            catch (Exception ex)
+            {
+                return Response<List<StudentViewModel>>.FailureResponse(ex);
+            }
+        }
+
+        [HttpGet("GetStudentById")]
+        public async Task<ActionResult<Response<StudentViewModel>>> GetStudentById(
+            [FromQuery] int Id
+        )
+        {
+            try
+            {
+                var student = await _db
+                    .Users.Where(u => u.Id == Id && u.Role == Role.Student && u.DeletedAt == null)
+                    .Include(u => u.Processes)
+                    .ThenInclude(p => p.University)
+                    .Include(u => u.Processes)
+                    .ThenInclude(p => p.UniversityProgram)
+                    .Include(u => u.Processes)
+                    .ThenInclude(p => p.Requirements)
+                    .SingleOrDefaultAsync();
+                if (student == null)
+                {
+                    return Response<StudentViewModel>.FailureResponse(
+                        $"Student with Id {Id} does not exist"
+                    );
+                }
+                return Response<StudentViewModel>.SuccessResponse(
+                    _mapper.Map<StudentViewModel>(student)
+                );
+            }
+            catch (Exception ex)
+            {
+                return Response<StudentViewModel>.FailureResponse(ex);
+            }
+        }
+    }
+}
--- frontend/app/http/ApiService.js
+++ frontend/app/http/ApiService.js
@@ -1,3 +1,6 @@
+'use client'
+
+import { showAlert } from '@/components/Alert';
 import { Api } from '../../types/api/Api';
 import { baseUrl } from './_config';
 
@@ -8,8 +11,71 @@ const requestInterceptor = (
     return config;
 };
 
+const successResponseInterceptor = (response) => {
+    // in case of response type set to Blob
+    if (
+        response.data &&
+        response.data instanceof Blob &&
+        response.data.type === 'application/json' // type = json implies error
+    ) {
+        response.data
+            .text()
+            .then((txt) => {
+                showAlert(txt.message, 'error');
+
+            })
+            .catch(() => {
+                response.data = {};
+            });
+    }
+
+    // normal case i.e. response type set to json
+    const success = response.data.success;
+    if (success === false) {
+        const errs = response.data.message;
+        showAlert(errs, 'error');
+
+    }
+    return response;
+};
+const errorResponseInterceptor = (error) => {
+    if (error.status === 401 || error.response?.status === 401) {
+        localStorage.removeItem('token');
+        window.location = "http://localhost:3000/"
+    } else {
+        if (error.response) {
+            if (error.response.data.success === false) {
+                showAlert(err.message, 'error');
+            }
+            else if (error.response.data.traceId) {
+                const errs = error.response.data.errors;
+                const errorKeys = Object.keys(errs).filter(
+                    (key) => !key.startsWith('$'),
+                );
+                const errorMsgs = []
+                for (const key of errorKeys) {
+                    for (const errMsg of errs[key]) {
+                        errorMsgs.push(errMsg);
+                    }
+                }
+                showAlert(errorMsgs.join('\n'), 'error');
+            } else {
+                showAlert(error.message, 'error');
+            }
+        } else {
+            showAlert(
+                error.message ??
+                'An error has occured. Please try again later.',
+                'error',
+            );
+        }
+    }
+};
+
 const apiService = new Api({ baseURL: baseUrl });
 
 apiService.instance.interceptors.request.use(requestInterceptor);
+apiService.instance.interceptors.response.use(successResponseInterceptor,
+    errorResponseInterceptor,)
 
 export default apiService;
